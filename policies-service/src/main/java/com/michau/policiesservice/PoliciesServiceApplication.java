d0e3216d8363306b1ee432564e1894455ee54c11
apokalipsa44 <m_lachowicz@wp.pl>
2022-08-18 01:07:10 +0200

    initial commit

---
+++ policies-service/src/main/java/com/michau/policiesservice/PoliciesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.michau.policiesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliciesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliciesServiceApplication.class, args);
+	}
+
+}
---
+++ policies-service/src/test/java/com/michau/policiesservice/PoliciesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michau.policiesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliciesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
